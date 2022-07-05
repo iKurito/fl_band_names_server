64b24f847b97aabb94dfb0ff5ee37c39b417882e
Ronaldo <ronaldo.tunque@pucp.edu.pe>
2022-07-05 14:40:52 -0500

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,24 @@
 const { io } = require("../index");
 
+const Bands = require("../models/bands");
+const Band = require("../models/band");
+
+const bands = new Bands();
+
+bands.addBand(new Band("Metallica"));
+bands.addBand(new Band("Iron Maiden"));
+bands.addBand(new Band("AC/DC"));
+bands.addBand(new Band("Nirvana"));
+
+// io => Soy yo como servidor express.js
+// client => Es el flutter que se conecta
+
 // Mensajes de Sockets
 io.on("connection", (client) => {
 	console.log("Cliente conectado");
+
+	client.emit("active-bands", bands.getBands());
+
 	client.on("disconnect", () => {
 		console.log("Cliente desconectado");
 	});
@@ -11,4 +27,25 @@ io.on("connection", (client) => {
 		console.log(payload);
 		io.emit("mensaje", { admin: "Nuevo mensaje" });
 	});
+
+	client.on("vote-band", (payload) => {
+		bands.voteBand(payload.id);
+		io.emit("active-bands", bands.getBands());
+	});
+
+	client.on("add-band", (payload) => {
+		bands.addBand(new Band(payload.name));
+		io.emit("active-bands", bands.getBands());
+	});
+
+	client.on("delete-band", (payload) => {
+		bands.deleteBand(payload.id);
+		io.emit("active-bands", bands.getBands());
+	});
+
+	// Cuando escucho al cliente (client.on(...)), yo (io.emit(...)) emito una respuesta
+	// client.on("emitir-mensaje", (payload) => {
+	// 	// io.emit("nuevo-mensaje", payload); // emite a todos
+	// 	client.broadcast.emit("nuevo-mensaje", payload); // emite a todos excepto al que lo envia
+	// });
 });
